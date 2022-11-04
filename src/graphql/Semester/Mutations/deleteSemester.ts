09a288be66599962c326676a04dd7c2c41729a74
manudelca <manueldelcarril@gmail.com>
2022-11-03 21:37:37 -0300

    Agregado el errorMsg

--- src/graphql/Semester/Mutations/deleteSemester.ts
+++ src/graphql/Semester/Mutations/deleteSemester.ts
@@ -15,8 +15,6 @@ export const deleteSemester = {
     }
   },
   resolve: async (_: undefined, { uuid }: IDeleteSemester) => {
-    // Devuelve 500 este findByUuid. Como hago para que el error de not found se vea mejor?
-    // En los otros ejemplos por lo que vi se hace igual que esto
     await SemesterRepository.findByUuid(uuid);
     await Database.transaction(async transaction => {
       await AnswerRepository.deleteBySemesterUuid(uuid, transaction);
