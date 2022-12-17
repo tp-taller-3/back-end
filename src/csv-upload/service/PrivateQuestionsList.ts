9cc9749fc8a41abd0688375cddf9ffd4848efd32
gianfranco <gconti@fi.uba.ar>
2022-12-17 15:04:11 -0300

    Better free text detection.

---
+++ src/csv-upload/service/PrivateQuestionsList.ts
@@ -0,0 +1,10 @@
+export const PRIVATE_QUESTIONS_LIST = [
+  "¿Cuáles fueron estos contenidos que entendés que te faltaron o necesitabas reforzar de forma previa?",
+  "¿Cuáles fueron estos contenidos y en qué otra materia fueron dictados?",
+  "¿En qué aspectos no se cumplió?",
+  "¿En qué forma no se cumplió?",
+  "¿Cuáles fueron los temas donde el tiempo no resultó suficiente?",
+  "¿Cuáles fueron los conocimientos no adquiridos?",
+  "Contanos",
+  "Comentarios:"
+];
